137f016b6c05422bdfe8b0993ffa669f2cf421a0
TranTrungNhan <trantrungnhan3039@gmail.com>
2024-10-21 20:20:06 -0700

    cau 5

--- cau5.sh
+++ cau5.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# This script creates 5 files named user2_N.txt (N from 1 to 5)
-# and writes "user 2 init" into each of them.
 
+# Tạo 5 file có tên user2_N.txt (với N từ 1 đến 5)
 for N in {1..5}; do
+  touch "user2_$N.txt"
+  
+  # Nếu N là số lẻ thì thêm nội dung "user 2 init" vào file
+  if [ $((N % 2)) -ne 0 ]; then
     echo "user 2 init" > "user2_$N.txt"
+  fi
 done
 
